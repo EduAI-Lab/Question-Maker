9bf91bd710732ba2264b296ca26c22aa3d6ec43d
ammaarkhan <ammaarkhan03@gmail.com>
2025-11-23 16:33:40 -0800

    fix ui bug

--- app/frontend/src/main.tsx
+++ app/frontend/src/main.tsx
@@ -5,9 +5,9 @@ import './index.css'
 import { ThemeProvider } from './components/theme-provider'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="eduquery-theme">
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <ThemeProvider defaultTheme="light" storageKey="eduquery-theme">
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>,
 ) 
